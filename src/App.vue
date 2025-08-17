36399e3fca314d66e236f6265da6234ee21278f5
ssgsg <986843038@qq.com>
2025-08-17 22:04:55 +0800

    canvus初试

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>hello,webgl</h1>
+  <canvas width="800" height="800" ref="canvasRef" class="canv"></canvas>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import missW from './assets/missW.jpg';
+
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+const img = new Image()
+img.src = missW
+
+
+onMounted(() => {
+  if (canvasRef.value instanceof HTMLCanvasElement) { // 类型守卫
+    const ctx = canvasRef.value.getContext('2d');
+    if (ctx) {
+      img.onload = function () {
+        ctx.drawImage(img, 0, 0)
+      }
+    }
+  }
+});
+</script>
+
+
+
+<style scoped>
+canvas {
+  background: wheat;
+}
+</style>
