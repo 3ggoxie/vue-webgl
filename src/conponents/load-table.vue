27cde29e8be5df9806e5f39f704488e31d3744e2
三告 <986843038@qq.com>
2025-09-01 16:08:43 +0800

    合并单元格

--- src/conponents/load-table.vue
+++ src/conponents/load-table.vue
@@ -1,74 +1,75 @@
 <template>
   <div>
-    <el-table :data="tableData" style="width: 100%" row-key="id" border lazy :load="load"
-      :tree-props="{ children: 'children', hasChildren: 'hasChildren' }">
-      <el-table-column prop="date" label="Date" />
+    <el-table
+      :data="tableData"
+      :span-method="objectSpanMethod"
+      border
+      style="width: 100%; margin-top: 20px"
+    >
+      <el-table-column prop="id" label="ID" width="180" />
       <el-table-column prop="name" label="Name" />
-      <el-table-column prop="address" label="Address" />
+      <el-table-column prop="amount1" label="Amount 1" />
     </el-table>
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { TableColumnCtx } from 'element-plus'
+
 interface User {
-  id: number
-  date: string
+  id: string
   name: string
-  address: string
-  hasChildren?: boolean
-  children?: User[]
+  amount1: string
 }
 
-const load = (
-  row: User,
-  treeNode: unknown,
-  resolve: (data: User[]) => void
-) => {
-  setTimeout(() => {
-    resolve([
-      {
-        id: 31,
-        date: '2016-05-01',
-        name: 'wangxiaohu',
-        address: 'No. 189, Grove St, Los Angeles',
-      },
-      {
-        id: 32,
-        date: '2016-05-01',
-        name: 'wangxiaohu',
-        address: 'No. 189, Grove St, Los Angeles',
-      },
-    ])
-  }, 1000)
+interface SpanMethodProps {
+  row: User
+  column: TableColumnCtx<User>
+  rowIndex: number
+  columnIndex: number
 }
 
-
+const objectSpanMethod = ({ rowIndex, columnIndex }: SpanMethodProps) => {
+  if (columnIndex === 0) {
+    if (rowIndex % 2 === 0) {
+      return {
+        rowspan: 2,
+        colspan: 1,
+      }
+    } else {
+      return {
+        rowspan: 0,
+        colspan: 0,
+      }
+    }
+  }
+}
 
 const tableData: User[] = [
   {
-    id: 1,
-    date: '2016-05-02',
-    name: 'wangxiaohu',
-    address: 'No. 189, Grove St, Los Angeles',
+    id: '12987122',
+    name: 'Tom',
+    amount1: '234',
   },
   {
-    id: 2,
-    date: '2016-05-04',
-    name: 'wangxiaohu',
-    address: 'No. 189, Grove St, Los Angeles',
+    id: '12987123',
+    name: 'Tom',
+    amount1: '165',
   },
   {
-    id: 3,
-    date: '2016-05-01',
-    name: 'wangxiaohu',
-    hasChildren: true,
-    address: 'No. 189, Grove St, Los Angeles',
+    id: '12987124',
+    name: 'Tom',
+    amount1: '324',
   },
   {
-    id: 4,
-    date: '2016-05-03',
-    name: 'wangxiaohu',
-    address: 'No. 189, Grove St, Los Angeles',
+    id: '12987125',
+    name: 'Tom',
+    amount1: '621',
+  },
+  {
+    id: '12987126',
+    name: 'Tom',
+    amount1: '539',
   },
 ]
 </script>
